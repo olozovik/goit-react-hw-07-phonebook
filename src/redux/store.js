189335a8617519844c8e175a220ad8dce8462992
Oleg Lozovik <o.lozovik@gmail.com>
2021-10-14 14:45:02 +0300

    Removed redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,44 +1,18 @@
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { phonebookReducer } from './phonebook/phonebook-reducer';
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 
-const phonebookPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
 const middleware = getDefaultMiddleware => {
   if (process.env.NODE_ENV === 'development') {
-    return [
-      ...getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
-      logger,
-    ];
+    return [...getDefaultMiddleware(), logger];
   }
   return getDefaultMiddleware();
 };
 
 const store = configureStore({
-  reducer: persistReducer(phonebookPersistConfig, phonebookReducer),
+  reducer: phonebookReducer,
   devTools: process.env.NODE_ENV === 'development',
   middleware,
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export { store };
